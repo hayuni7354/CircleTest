97e0e477d8a54eed05ab00538e72446662ea6b3c
unknown <kimdogyun0906@gmail.com>
2024-12-24 16:47:30 +0900

    result screen done, leaderboard ui(wip)

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pygame
 import math
 import time
 import numpy as np
+import heapq
+import copy
 pygame.init() #초기화
 
 #화면 크기 설정
-g_screen_width = 1200 # 가로 크기
-g_screen_height = 900 # 세로 크기
+g_screen_width = 1280 # 가로 크기
+g_screen_height = 1024 # 세로 크기
 g_screen = pygame.display.set_mode((g_screen_width, g_screen_height), pygame.SCALED)
 
 #화면 타이틀 설정
@@ -16,15 +18,23 @@ pygame.display.set_caption("CircleTest")
 TITLEFONT = pygame.font.SysFont( "malgungothic", 50, True, False)
 TEXTFONT = pygame.font.SysFont( "malgungothic", 30, False, False)
 WHITE = (255, 255, 255)
+GREEN = (0, 255, 0) 
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GRAY = (127, 127, 127)
+LIGHT_RED = (255, 127, 127)
 
 #게임 설정
 MAXSCORE = 999 # 최대 점수
 
+#전역변수들
+g_bestAcc = 0 # 최고 정확도
+g_bestDrawPoints = [] # 최고 정확도를 얻은 원
+
 # numpy float 출력 옵션 변경(소수점 3자리까지 출력, array의 원소 값 자체가 변경되지는 않음)
 #np.set_printoptions(precision=3, suppress=True)
 
+#TODO : 마우스를 누른 채로 버튼 위로 올려도 버튼이 눌리는 문제 해결
 class TextButten: # 글자 버튼
     def __init__(self, text, center, action = None, condition = True, size = [300,100]):
         if(condition):
@@ -46,7 +56,23 @@ class TextButten: # 글자 버튼
         g_screen.blit(text_Title, text_Rect)
         pygame.draw.rect(g_screen, color, [center[0] - size[0]/2, center[1] - size[1]/2, size[0], size[1]], 3)
 
+class ScoreBoard:
+    def __init__(self):
+        self.data = []
+        self.nextID = 0
+    def postScore(self, acc, drawPoints, namePoints): # 정확도, 그림, 이름으로 점수 등록
+        heapq.heappush(self.data, [-acc, drawPoints, namePoints, self.nextID])
+        self.nextID += 1
+        return self.nextID
+    def searchRankbyNewAcc(self, acc): # acc가 점수표에 추가된다면 몇 등일지를 반환
+        temp = copy.deepcopy(self.data)
+        heapq.heappush(temp, [-acc, [], [], -1])
+        for i in range(1, len(temp) + 1):
+            if(heapq.heappop(temp)[3] == -1):
+                return i
 
+
+g_scoreBoard = ScoreBoard()
 
 def changeScene(scene): # 인자는 인스턴스가 아닌 클래스로 주어져야 한다
     global g_game
@@ -58,8 +84,8 @@ def angleSubtract(angle1, angle2): #angle1에서 angle2을 뺌 (라디안)
     if(math.copysign(1, angle1) == math.copysign(1, angle2)): # 부호가 같거나 하나가 0이면
         return sub
     else:
-        if(abs(angle1 - angle2) > math.pi):
-            return (sub - math.copysign(2*math.pi, sub))
+        if(abs(angle1 - angle2) > math.pi): # 각의 차이가 180도를 넘으면
+            return (sub - math.copysign(2*math.pi, sub)) # 특수한 경계 넘었다고 생각, 보정
         else:
             return sub
         
@@ -92,8 +118,8 @@ class TitleScene: # 타이틀 화면
 
         # 버튼들
         startButten = TextButten("시작하기!", [g_screen_width/2, 675], lambda: changeScene(IngameScene))
-        boardButten = TextButten("점수판", [g_screen_width/2 + 350, 675])
-        htpButten = TextButten("게임 방법", [g_screen_width/2 - 350, 675])
+        boardButten = TextButten("점수판", [g_screen_width/2 + 350, 675], lambda: changeScene(ScoreBoardScene))
+        creditButten = TextButten("크레딧", [g_screen_width/2 - 350, 675])
 
 class IngameScene: # 인게임 화면
     def __init__(self):
@@ -101,7 +127,8 @@ class IngameScene: # 인게임 화면
         self.calcPoints = [] # 계산되는 위치들
 
         self.isDrawMode = False # 그리는 중 여부
-        self.gameEndID = -1 # 게임 종료 아이디 (-1 : 게임중 or 초기상태 / 0 : 정상종료)
+        self.gameEndID = -1 # 게임 종료 아이디 (-1 : 게임중 or 초기상태 / 0 : 정상종료 / 1 : 너무 가까움 / 2 : 상자 벗어남 / 3 : 잘못된 방향 / 4 : 그리다 멈춤)
+        self.isNewRecord = False # 게임 종료시 신기록 달성 여부
 
         #모든 각은 라디안
         self.isDrawOverHalf = False # 원을 절반 넘게 그렸는지 여부
@@ -112,17 +139,20 @@ class IngameScene: # 인게임 화면
     def event(self, event):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        if(event.type == pygame.MOUSEBUTTONDOWN and (40 < mouse[0] < g_screen_height - 50 and 40 < mouse[1] < g_screen_height - 50)):
+        if(event.type == pygame.MOUSEBUTTONDOWN and (40 < mouse[0] < g_screen_height - 100 and 40 < mouse[1] < g_screen_height - 100)):
             if((mouse[0] - g_screen_height/2)**2 + abs(mouse[1] - g_screen_height/2)**2 < 90**2): # 중점에서 반경 90픽셀 원 안 -> 너무 가까움
-                print('tooclose')
+                self.gameEndID = 1
+                self.isDrawMode = False
             self.__init__()
             self.isDrawMode = True
         if(event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEMOTION):
             if(click[0] and self.isDrawMode):
                 if((mouse[0] - g_screen_height/2)**2 + abs(mouse[1] - g_screen_height/2)**2 < 30**2): # 중점에서 반경 30픽셀 원 안 -> 너무 가까움
-                    print('tooclose')
-                elif(not (40 < mouse[0] < g_screen_height - 50 and 40 < mouse[1] < g_screen_height - 50)): # 그릴수 있는 하얀 박스 밖 -> 너무 멂
-                    print('toofar')
+                    self.gameEndID = 1
+                    self.isDrawMode = False
+                elif(not (40 < mouse[0] < g_screen_height - 100 and 40 < mouse[1] < g_screen_height - 100)): # 그릴수 있는 하얀 박스 밖 -> 상자 벗어남
+                    self.gameEndID = 2
+                    self.isDrawMode = False
                 else:
                     if(not self.drawPoints):
                         self.firstPointAngle = math.atan2(-(mouse[1] - g_screen_height/2), mouse[0] - g_screen_height/2)
@@ -139,7 +169,13 @@ class IngameScene: # 인게임 화면
                             self.isDrawClockwise = False
                     elif(175 < angleDiff < 185): # 180도 돔 (범위 +-5도)
                         self.isDrawOverHalf = True
-                    #TODO : 여기도 끝남 검사
+                    # 한바퀴에서 10도 초과로 넘침 -> 다 그린 것으로
+                    if(self.isDrawOverHalf):
+                        if(self.isDrawClockwise and -170 < angleDiff < -10):
+                            self.gameEnd0()
+                        elif(self.isDrawClockwise is False and 170 > angleDiff > 10):
+                            print(angleDiff)
+                            self.gameEnd0()
                     
                     # angleDiff = lastPointAngle과의 각 (도)
                     angleDiff = math.degrees(angleSubtract(angle, self.lastPointAngle))
@@ -147,12 +183,14 @@ class IngameScene: # 인게임 화면
                         if(angleDiff < 0): # 도는방향과 같은방향으로 더 돌음 -> 값 업데이트
                             self.lastPointAngle = angle
                         if(angleDiff > 1): # 도는방향과 반대방향으로 1도 벗어남 -> 잘못된 방향
-                            print('wrongway')
+                            self.gameEndID = 3
+                            self.isDrawMode = False
                     elif(self.isDrawClockwise is False):
                         if(angleDiff > 0):
                             self.lastPointAngle = angle
                         elif(angleDiff < -1):
-                            print('wrongway')
+                            self.gameEndID = 3
+                            self.isDrawMode = False
 
                     # 그린 점 추가
                     self.calcPoints.append([mouse[0] - g_screen_height/2, -(mouse[1] - g_screen_height/2)])
@@ -174,10 +212,13 @@ class IngameScene: # 인게임 화면
 
                 # 한바퀴에서 10도 미만으로 모자람 -> 다 그린 것으로
                 if(self.isDrawClockwise and angleDiff < 10):
-                    print('end')
+                    self.gameEnd0()
+                    return
                 elif(self.isDrawClockwise is False and angleDiff > -10):
-                    print('end')
-            print('stopdraw')
+                    self.gameEnd0()
+                    return
+            self.gameEndID = 4 # 다안그렸는데 뗌 -> 그리다 멈품
+            self.isDrawMode = False
 
     def draw(self):
         for i in range(len(self.drawPoints)): # 그림
@@ -187,35 +228,78 @@ class IngameScene: # 인게임 화면
         #for i in range(len(self.calcPoints)): # 계산된 점 그림(디버그용)
         #    pygame.draw.circle(screen, [255,255,0], [self.calcPoints[i][0] + screen_height/2, screen_height/2 - self.calcPoints[i][1]], 1, 1)
         pygame.draw.circle(g_screen, GRAY, [g_screen_height/2, g_screen_height/2], 10, 10) # 중점
-        pygame.draw.rect(g_screen, WHITE, [30, 30, g_screen_height - 70, g_screen_height - 70], 3) # 그릴수 있는 하얀 박스
+        pygame.draw.rect(g_screen, WHITE, [30, 30, g_screen_height - 120, g_screen_height - 120], 3) # 그릴수 있는 하얀 박스
 
         # 정확도 글자
         acc = self.getAcc()
+        color = None
+        if(self.isNewRecord): color = RED
+        else: color = GREEN
         if(not math.isnan(acc)):
-            accText = TEXTFONT.render(f'{acc*100 : .1f}%', True, WHITE)
+            #TODO : 텍스트 관련 변수들 이름 앞 acc 제거
+            accText = TEXTFONT.render(f'{acc*100 : .1f}%', True, color)
             accTextRect = accText.get_rect()
             accTextRect.centerx = g_screen_height/2 - 5
             accTextRect.centery = g_screen_height/2 - 50
             g_screen.blit(accText, accTextRect)
 
-        #if(self.gameEndID == 0):
-        if(not math.isnan(acc)):
-            accText = TEXTFONT.render(f'점수', True, WHITE)
+        if(self.gameEndID == 0):
+        #if(not math.isnan(acc)):
+            accText = TEXTFONT.render(f'{toScore(acc) : .0f}점', True, color)
             accTextRect = accText.get_rect()
             accTextRect.centerx = g_screen_height/2
             accTextRect.centery = g_screen_height/2 + 50
             g_screen.blit(accText, accTextRect)
-            accText = TEXTFONT.render(f'{toScore(acc) : .0f}', True, WHITE)
+
+            if(self.isNewRecord):
+                accText = TEXTFONT.render('NEW RECORD!', True, color)
+                accTextRect = accText.get_rect()
+                accTextRect.centerx = g_screen_height/2
+                accTextRect.centery = g_screen_height/2 + 100
+                g_screen.blit(accText, accTextRect)
+
+                accText = TEXTFONT.render('점수를 저장하려면 점수등록 버튼을 눌러주세요', True, RED)
+                accTextRect = accText.get_rect()
+                accTextRect.centerx = g_screen_height/2
+                accTextRect.centery = g_screen_height/2 + 180
+                g_screen.blit(accText, accTextRect)
+
+        elif(self.gameEndID == -1 and not self.isDrawMode):
+            accText = TEXTFONT.render('회색 점을 중심으로 여기에 그려주세요', True, WHITE)
             accTextRect = accText.get_rect()
-            accTextRect.centerx = g_screen_height/2 - 5
-            accTextRect.centery = g_screen_height/2 + 90
+            accTextRect.centerx = g_screen_height/2
+            accTextRect.centery = g_screen_height/2 + 50
+            g_screen.blit(accText, accTextRect)
+        elif(self.gameEndID == 1):
+            accText = TEXTFONT.render('점과 거리를 두고 그려주세요', True, LIGHT_RED)
+            accTextRect = accText.get_rect()
+            accTextRect.centerx = g_screen_height/2
+            accTextRect.centery = g_screen_height/2 + 50
+            g_screen.blit(accText, accTextRect)
+        elif(self.gameEndID == 2):
+            accText = TEXTFONT.render('흰 상자 안에 그려주세요', True, LIGHT_RED)
+            accTextRect = accText.get_rect()
+            accTextRect.centerx = g_screen_height/2
+            accTextRect.centery = g_screen_height/2 + 50
+            g_screen.blit(accText, accTextRect)
+        elif(self.gameEndID == 3):
+            accText = TEXTFONT.render('한쪽 방향으로만 그려주세요', True, LIGHT_RED)
+            accTextRect = accText.get_rect()
+            accTextRect.centerx = g_screen_height/2
+            accTextRect.centery = g_screen_height/2 + 50
+            g_screen.blit(accText, accTextRect)
+        elif(self.gameEndID == 4):
+            accText = TEXTFONT.render('원을 끝까지 그려주세요', True, LIGHT_RED)
+            accTextRect = accText.get_rect()
+            accTextRect.centerx = g_screen_height/2
+            accTextRect.centery = g_screen_height/2 + 50
             g_screen.blit(accText, accTextRect)
 
         pygame.draw.rect(g_screen, WHITE, [g_screen_width - 330, 30, 300, 600], 3) # 리더보드
         backButten = TextButten("돌아가기", [g_screen_width - 180, 700], lambda: changeScene(TitleScene)) # 돌아가기 버튼
-        backButten = TextButten("점수등록", [g_screen_width - 180, 820], lambda: changeScene()) # 점수등록 버튼
+        postScoreButten = TextButten("점수등록", [g_screen_width - 180, 820], lambda: changeScene(PostScoreScene), self.gameEndID == 0) # 점수등록 버튼
 
-    def getAcc(self):
+    def getAcc(self): # calcPoints로 정확도를 계산하는 함수
         if(len(self.calcPoints) <= 1):
             return math.nan
         distances = []
@@ -223,6 +307,77 @@ class IngameScene: # 인게임 화면
             distances.append((self.calcPoints[i][0]**2 + self.calcPoints[i][1]**2)**0.5)
         return 1 - np.std(distances)/np.mean(distances)
     
+    def gameEnd0(self): # 게임 정상 종료 시 처리를 하는 함수
+        self.gameEndID = 0
+        self.isDrawMode = False
+        acc = self.getAcc()
+        global g_bestAcc
+        global g_bestDrawPoints
+        if(acc > g_bestAcc):
+            self.isNewRecord = True
+            g_bestAcc = acc
+            g_bestDrawPoints = self.drawPoints
+
+class PostScoreScene: # 점수등록 화면
+    def __init__(self):
+        self.namePoints = [] # 이름 쓴 위치들
+        self.rank = g_scoreBoard.searchRankbyNewAcc(g_bestAcc) # 최고점수가 추가될 경우의 순위
+    def event(self, event):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if(event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEMOTION):
+            if(click[0]):
+                if((40 < mouse[0] < 780 and 660 < mouse[1] < 900)): # 그릴수 있는 하얀 박스 밖 -> 상자 벗어남
+                    # 그린 점 추가
+                    self.namePoints.append([mouse[0] - 30, mouse[1] - (g_screen_height/2 + 160)])
+    def draw(self):
+        for i in range(len(g_bestDrawPoints)): # 그림(1/2 축척, (15,15) 평행이동)
+            pygame.draw.circle(g_screen, WHITE, [g_bestDrawPoints[i][0]/2 + 15, g_bestDrawPoints[i][1]/2+ 15], 5, 5)
+            if(i != 0):
+               pygame.draw.line(g_screen, WHITE, [g_bestDrawPoints[i-1][0]/2 + 15, g_bestDrawPoints[i-1][1]/2 + 15], [g_bestDrawPoints[i][0]/2 + 15, g_bestDrawPoints[i][1]/2 + 15], 12)
+        pygame.draw.circle(g_screen, GRAY, [g_screen_height/4 + 15, g_screen_height/4 + 15], 5, 5) # 중점
+        pygame.draw.rect(g_screen, WHITE, [30, 30, (g_screen_height - 70)/2  + 15, (g_screen_height - 70)/2 + 15], 3) # 그릴수 있는 하얀 박스(였던것)
+
+        accText = TITLEFONT.render(f'최고 정확도 : {g_bestAcc*100 : .1f}%', True, WHITE)
+        g_screen.blit(accText, ((g_screen_height - 70)/2 + 80, 100))
+        accText = TITLEFONT.render(f'최고 점수 : {toScore(g_bestAcc) : .0f}점', True, WHITE)
+        g_screen.blit(accText, ((g_screen_height - 70)/2 + 80, 180))
+        accText = TITLEFONT.render(f'현재 순위 : {self.rank : .0f}등', True, WHITE)
+        g_screen.blit(accText, ((g_screen_height - 70)/2 + 80, 260))
+
+        accText = TITLEFONT.render('이름을 써 주세요', True, WHITE)
+        g_screen.blit(accText, (50, 550))
+        pygame.draw.rect(g_screen, WHITE, [30, 650, 760, 260], 3) #이름 쓰는 하얀 박스
+        for i in range(len(self.namePoints)): # 이름
+            pygame.draw.circle(g_screen, WHITE, [self.namePoints[i][0] + 30, self.namePoints[i][1] + (g_screen_height/2 + 160)], 5, 5)
+
+        nameResetButten = TextButten("이름 초기화", [g_screen_width - 180, 580], lambda: self.resetName()) # 이름리셋 버튼
+        backButten = TextButten("돌아가기", [g_screen_width - 180, 700], lambda: changeScene(IngameScene)) # 돌아가기 버튼
+        postScoreButten = TextButten("이름등록", [g_screen_width - 180, 820], lambda: self.postScore(), self.namePoints) # 등록완료 버튼
+    def resetName(self):
+        self.namePoints = []
+    def postScore(self): # 점수 등록
+        global g_scoreBoard
+        global g_bestAcc
+        global g_bestDrawPoints
+        g_scoreBoard.postScore(g_bestAcc, g_bestDrawPoints, self.namePoints)
+        g_bestAcc = 0
+        g_bestDrawPoints = []
+        changeScene(TitleScene)
+
+class ScoreBoardScene():
+    def __init__(self):
+        pass
+    def event(self, event):
+        pass
+    def draw(self):
+        pygame.draw.rect(g_screen, WHITE, [30, 30, g_screen_width - 60, g_screen_height - 220], 3) # 점수표 상자
+        backButten = TextButten("돌아가기", [g_screen_width - 180, 920], lambda: changeScene(TitleScene)) # 돌아가기 버튼
+        for i in range(5):
+            text = TITLEFONT.render(f'999', True, YELLOW)
+            g_screen.blit(text, (70, 100 + 100*i))
+            for j in range(len(g_scoreBoard.data[0][2])): # 이름
+                pygame.draw.circle(g_screen, WHITE, [g_scoreBoard.data[0][2][j][0]/4 + 170, g_scoreBoard.data[0][2][j][1]/4 + 100], 5, 5)
 
 
 #이벤트 루프
